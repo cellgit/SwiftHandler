721d660e8960f5b933d37211eb90738c062eca87
liuhongli <>
2024-09-03 20:25:17 +0800

    [t0]OCR提取pdf文本

--- Sources/SwiftHandler/Components/PDF/PDFKitView.swift
+++ Sources/SwiftHandler/Components/PDF/PDFKitView.swift
@@ -27,12 +27,21 @@ public struct PDFKitView: View {
         PDFKitRepresentedView(url)
             .edgesIgnoringSafeArea(.all)
             .onAppear {
+#if os(macOS)
+                extractTextUsingOCR(from: url)
+#else
                 extractTextFromPDF(with: url)
-//                extractTextUsingOCR(from: url)
+#endif
+                
+                
+                
             }
             .onChange(of: url) { oldValue, newValue in
+#if os(macOS)
+                extractTextUsingOCR(from: newValue)
+#else
                 extractTextFromPDF(with: newValue)
-//                extractTextUsingOCR(from: newValue)
+#endif
             }
             .onChange(of: extractedText) { oldValue, newValue in
                 if oldValue != newValue {
@@ -66,68 +75,72 @@ public struct PDFKitView: View {
 }
 
 
-//extension PDFKitView {
-//
-//    private func extractTextUsingOCR(from pdfUrl: URL) -> String {
-//        guard let pdfDocument = PDFDocument(url: pdfUrl) else {
-//            return ""
-//        }
-//        
-//        for pageIndex in 0..<pdfDocument.pageCount {
-//            guard let page = pdfDocument.page(at: pageIndex) else { continue }
-//            
-//            let pageBounds = page.bounds(for: .mediaBox)
-//            let pageImage = page.thumbnail(of: pageBounds.size, for: .mediaBox)
-//            
-//            // 将NSImage转换为CGImage
-//            guard let cgImage = pageImage.cgImage(forProposedRect: nil, context: nil, hints: nil) else { continue }
-//            
-//            let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-//            let request = VNRecognizeTextRequest { (request, error) in
-//                guard let observations = request.results as? [VNRecognizedTextObservation] else {
-//                    return
-//                }
-//                let pageText = observations.compactMap { $0.topCandidates(1).first?.string }.joined(separator: " ")
-//                extractedText += pageText + "\n"
-//                debugPrint("extractedText === \(extractedText)")
-//            }
-//            
-//            do {
-//                try requestHandler.perform([request])
-//            } catch {
-//                print("Error performing OCR: \(error)")
-//            }
-//        }
-//        
-//        debugPrint("extractedText2 === \(extractedText)")
-//        return extractedText
-//    }
-//    
-//    private func performTextRecognition(on cgImage: CGImage, completion: @escaping (Result<String, Error>) -> Void) {
-//        let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-//        let request = VNRecognizeTextRequest { (request, error) in
-//            guard error == nil else {
-//                completion(.failure(error!))
-//                return
-//            }
-//            if let observations = request.results as? [VNRecognizedTextObservation] {
-//                let text = observations.compactMap { $0.topCandidates(1).first?.string }.joined(separator: "\n")
-//                DispatchQueue.main.async {
-//                    completion(.success(text))
-//                }
-//            }
-//        }
-//        
-//        // 自动检测语言
-//        request.automaticallyDetectsLanguage = true
-//        request.usesLanguageCorrection = true
-//        
-//        DispatchQueue.global(qos: .userInitiated).async {
-//            do {
-//                try requestHandler.perform([request])
-//            } catch {
-//                completion(.failure(error))
-//            }
-//        }
-//    }
-//}
+#if os(macOS)
+
+extension PDFKitView {
+
+    private func extractTextUsingOCR(from pdfUrl: URL) -> String {
+        guard let pdfDocument = PDFDocument(url: pdfUrl) else {
+            return ""
+        }
+        
+        for pageIndex in 0..<pdfDocument.pageCount {
+            guard let page = pdfDocument.page(at: pageIndex) else { continue }
+            
+            let pageBounds = page.bounds(for: .mediaBox)
+            let pageImage = page.thumbnail(of: pageBounds.size, for: .mediaBox)
+            
+            // 将NSImage转换为CGImage
+            guard let cgImage = pageImage.cgImage(forProposedRect: nil, context: nil, hints: nil) else { continue }
+            
+            let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+            let request = VNRecognizeTextRequest { (request, error) in
+                guard let observations = request.results as? [VNRecognizedTextObservation] else {
+                    return
+                }
+                let pageText = observations.compactMap { $0.topCandidates(1).first?.string }.joined(separator: " ")
+                extractedText += pageText + "\n"
+                debugPrint("extractedText === \(extractedText)")
+            }
+            
+            do {
+                try requestHandler.perform([request])
+            } catch {
+                print("Error performing OCR: \(error)")
+            }
+        }
+        
+        debugPrint("extractedText2 === \(extractedText)")
+        return extractedText
+    }
+    
+    private func performTextRecognition(on cgImage: CGImage, completion: @escaping (Result<String, Error>) -> Void) {
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        let request = VNRecognizeTextRequest { (request, error) in
+            guard error == nil else {
+                completion(.failure(error!))
+                return
+            }
+            if let observations = request.results as? [VNRecognizedTextObservation] {
+                let text = observations.compactMap { $0.topCandidates(1).first?.string }.joined(separator: "\n")
+                DispatchQueue.main.async {
+                    completion(.success(text))
+                }
+            }
+        }
+        
+        // 自动检测语言
+        request.automaticallyDetectsLanguage = true
+        request.usesLanguageCorrection = true
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try requestHandler.perform([request])
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
+#endif
