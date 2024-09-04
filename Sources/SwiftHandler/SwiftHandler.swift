c34644add1c6d0132640ab174ba08b0416c2f944
liuhongli <liuhongli2120@gmail.com>
2024-09-04 17:40:31 +0800

    [t0]适配iPad

--- Sources/SwiftHandler/SwiftHandler.swift
+++ Sources/SwiftHandler/SwiftHandler.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+#if os(iOS)
+import UIKit
+
+public let isPad = UIDevice.current.userInterfaceIdiom == .pad
+
+#else
+
+#endif
